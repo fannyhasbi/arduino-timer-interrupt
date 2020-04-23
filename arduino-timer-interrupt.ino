2bae286b7891e9a197f137557c8ff965c0bd99c5
fannyhasbi <fnyhsbi@gmail.com>
2020-04-24 02:13:42 +0700

    test led pin

--- arduino-timer-interrupt.ino
+++ arduino-timer-interrupt.ino
@@ -1,6 +1,15 @@
+const int ledPin = 5;
+
 void setup() {
   Serial.begin(9600);
+  Serial.println("Timer Interrupt");
+
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
+  digitalWrite(ledPin, HIGH);
+  delay(1000);
+  digitalWrite(ledPin, LOW);
+  delay(1000);
 }
