ecfd57ccf85a70037c0d479d87d59e6fbc26a0fa
fannyhasbi <fnyhsbi@gmail.com>
2020-04-24 02:19:52 +0700

    counter value for 2 seconds

--- arduino-timer-interrupt.ino
+++ arduino-timer-interrupt.ino
@@ -2,9 +2,15 @@ const int ledPin = 5;
 const int risePin = 2;
 const int lowPin = 4;
 
+// 16MHz = 16x10^6 Hz
+// max of 0xFFFF is 65535
+// 65535 - ( 16x10^6 x 2 / 1024 )
+float counter = 34285; // 2 seconds
+
 void setup() {
   Serial.begin(9600);
   Serial.println("Timer Interrupt");
+  Serial.println(counter);
 
   pinMode(ledPin, OUTPUT);
   pinMode(risePin, INPUT);
